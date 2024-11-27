42a6185294b7526c8dc85867636acfbffd73b37e
ljxwin <2961184865@qq.com>
2024-11-27 11:42:00 +0800

    first commit

---
+++ serial-monitor/MFC_serial/resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCserial.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFC_SERIAL_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     131
+#define IDB_BMP_BK                      131
+#define IDC_EDIT1                       1000
+#define IDC_COMBO1                      1001
+#define IDC_EDIT2                       1002
+#define IDC_CHART                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_EDIT_P                      1008
+#define IDC_EDIT_I                      1010
+#define IDC_EDIT_D                      1011
+#define IDC_UPDATE                      1012
+#define IDC_EDIT7                       1013
+#define IDC_EDIT_DIS                    1013
+#define IDC_STATICU                     1014
+#define IDC_BUTTON2                     1015
+#define IDC_BUTTON4                     1017
+#define IDC_EDIT_P1                     1018
+#define IDC_EDIT_D1                     1019
+#define IDC_BTN_ABOUT                   1020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
